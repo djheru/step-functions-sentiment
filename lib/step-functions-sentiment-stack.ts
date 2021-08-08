8b1f82afb0eab926e527f18974829274a346a28c
Philip Damra <pdamra@dealerinspire.com>
2021-08-08 14:56:41 -0700

    Lambda to send the negative sentiment notification via SES

--- lib/step-functions-sentiment-stack.ts
+++ lib/step-functions-sentiment-stack.ts
@@ -1,9 +1,20 @@
+import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
 import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import { RetentionDays } from '@aws-cdk/aws-logs';
-import { LambdaInvoke } from '@aws-cdk/aws-stepfunctions-tasks';
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import {
+  Choice,
+  Condition,
+  JsonPath,
+  Succeed,
+} from '@aws-cdk/aws-stepfunctions';
+import {
+  DynamoAttributeValue,
+  DynamoPutItem,
+  LambdaInvoke,
+} from '@aws-cdk/aws-stepfunctions-tasks';
+import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 import { pascalCase } from 'change-case';
 
 export class StepFunctionsSentimentStack extends Stack {
@@ -15,6 +26,13 @@ export class StepFunctionsSentimentStack extends Stack {
 
   public generateReferenceNumberLambda: NodejsFunction;
   public generateReferenceNumber: LambdaInvoke;
+
+  public feedbackTable: Table;
+  public saveFeedback: DynamoPutItem;
+
+  public negativeSentimentNotificationLambda: NodejsFunction;
+  public sendSentimentNotification: LambdaInvoke;
+  public checkSentimentChoice: Choice;
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
@@ -56,6 +74,50 @@ export class StepFunctionsSentimentStack extends Stack {
     });
   }
 
+  buildSentimentNotificationLambda() {
+    const sendSentimentNotificationId = pascalCase(
+      `${this.id}-sentiment-notification`
+    );
+    const negativeSentimentNotificationLambdaId = pascalCase(
+      `${sendSentimentNotificationId}-lambda`
+    );
+    this.negativeSentimentNotificationLambda = new NodejsFunction(
+      this,
+      negativeSentimentNotificationLambdaId,
+      {
+        functionName: negativeSentimentNotificationLambdaId,
+        runtime: Runtime.NODEJS_12_X,
+        entry: 'src/handlers.ts',
+        handler: 'negativeSentimentNotification',
+        memorySize: 256,
+        logRetention: RetentionDays.ONE_MONTH,
+        bundling: {
+          nodeModules: ['aws-sdk', 'ulid'],
+          externalModules: [],
+        },
+      }
+    );
+
+    this.sendSentimentNotification = new LambdaInvoke(
+      this,
+      sendSentimentNotificationId,
+      {
+        lambdaFunction: this.negativeSentimentNotificationLambda,
+        resultPath: '$.notifyViaEmail',
+      }
+    );
+
+    this.checkSentimentChoice = new Choice(this, 'checkSentiment')
+      .when(
+        Condition.stringEquals(
+          '$.sentimentResult.Payload.Sentiment',
+          'NEGATIVE'
+        ),
+        this.sendSentimentNotification
+      )
+      .otherwise(new Succeed(this, 'positiveSentiment'));
+  }
+
   buildIdGeneratorLambda() {
     const generateReferenceNumberId = pascalCase(`${this.id}-generate-id`);
     const generateReferenceNumberLambdaId = pascalCase(
@@ -87,5 +149,34 @@ export class StepFunctionsSentimentStack extends Stack {
         resultPath: '$.ticketId',
       }
     );
+  }
+
+  buildFeedbackTable() {
+    const feedbackTableId = pascalCase(`${this.id}-feedback-table`);
+    const saveFeedbackId = pascalCase(`${this.id}-save-feedback`);
+    this.feedbackTable = new Table(this, feedbackTableId, {
+      partitionKey: {
+        name: 'formId',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    this.saveFeedback = new DynamoPutItem(this, saveFeedbackId, {
+      table: this.feedbackTable,
+      item: {
+        formId: DynamoAttributeValue.fromString(
+          JsonPath.stringAt('$.ticketId.Payload')
+        ),
+        customerMessage: DynamoAttributeValue.fromString(
+          JsonPath.stringAt('$.message')
+        ),
+        sentiment: DynamoAttributeValue.fromString(
+          JsonPath.stringAt('$.sentimentResult.Payload.Sentiment')
+        ),
+      },
+      resultPath: '$.formDataRecord',
+    });
   }
 }
--- src/handlers.ts
+++ src/handlers.ts
@@ -1,11 +1,18 @@
-import { Comprehend } from 'aws-sdk';
+import { Comprehend, SESV2 } from 'aws-sdk';
 import { ulid } from 'ulid';
 
 type Event = {
   message: string;
 };
 
+const {
+  AWS_REGION: region,
+  RECIPIENT: recipient = '',
+  SENDER: FromEmailAddress = '',
+} = process.env;
+
 const comprehend = new Comprehend({ apiVersion: '2017-11-27' });
+const ses = new SESV2({ region });
 
 export const sentimentHandler = async (event: Event) => {
   try {
@@ -26,4 +33,41 @@ export const sentimentHandler = async (event: Event) => {
 
 export const idGenerator = async () => {
   return ulid();
+};
+
+export const negativeSentimentNotification = async (event: any) => {
+  try {
+    const message = `
+      Sentiment analysis: ${event.sentimentResult.Payload.Sentiment}
+      Customer Review: ${event.message}
+    `;
+    await ses
+      .sendEmail({
+        FromEmailAddress,
+        Destination: {
+          ToAddresses: [recipient],
+        },
+        Content: {
+          Simple: {
+            Subject: {
+              Charset: 'UTF-8',
+              Data: 'Negative sentiment review submission',
+            },
+            Body: {
+              Text: {
+                Charset: 'UTF-8',
+                Data: message,
+              },
+            },
+          },
+        },
+      })
+      .promise();
+    return {
+      body: 'Notification submitted successfully!',
+    };
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
