809feb5a6d12223b75c32c4ceba408145159996e
Philip Damra <pdamra@dealerinspire.com>
2021-08-07 23:20:19 -0700

    Code for sentiment analysis handler

---
+++ src/handlers.ts
@@ -0,0 +1,23 @@
+import { Comprehend } from 'aws-sdk';
+type Event = {
+  message: string;
+};
+
+const comprehend = new Comprehend({ apiVersion: '2017-11-27' });
+
+export const sentimentHandler = async (event: Event) => {
+  try {
+    console.log('sentimentHandler: %j', event);
+    const data = await comprehend
+      .detectSentiment({
+        LanguageCode: 'en',
+        Text: event.message,
+      })
+      .promise();
+    console.log('Sentiment Analysis: %j', data);
+    return data;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
