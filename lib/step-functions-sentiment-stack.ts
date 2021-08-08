2eae0a533feeb5661c6a3ddfd89f40bf95377485
Philip Damra <pdamra@dealerinspire.com>
2021-08-08 00:16:43 -0700

    Stack method to assemble the sentiment lambda and step function task

--- lib/step-functions-sentiment-stack.ts
+++ lib/step-functions-sentiment-stack.ts
@@ -1,9 +1,55 @@
-import * as cdk from '@aws-cdk/core';
+import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import { RetentionDays } from '@aws-cdk/aws-logs';
+import { LambdaInvoke } from '@aws-cdk/aws-stepfunctions-tasks';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { pascalCase } from 'change-case';
 
-export class StepFunctionsSentimentStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class StepFunctionsSentimentStack extends Stack {
+  public id: string;
+  private props: StackProps;
+
+  public sentimentLambda: NodejsFunction;
+  public detectSentiment: LambdaInvoke;
+
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.id = id;
+    this.props = props;
+  }
+
+  buildSentimentLambda() {
+    const detectSentimentId = pascalCase(`${this.id}-sentiment`);
+    const sentimentLambdaId = pascalCase(`${detectSentimentId}-lambda`);
+
+    // Lambda function that invokes the Comprehend service
+    this.sentimentLambda = new NodejsFunction(this, sentimentLambdaId, {
+      functionName: sentimentLambdaId,
+      runtime: Runtime.NODEJS_12_X,
+      entry: 'src/handlers.ts',
+      handler: 'sentimentHandler',
+      memorySize: 256,
+      logRetention: RetentionDays.ONE_MONTH,
+      bundling: {
+        nodeModules: ['aws-sdk', 'ulid'],
+        externalModules: [],
+      },
+    });
+
+    // IAM permissions to allow the lambda to invoke Comprehend
+    const allowComprehend = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['comprehend:DetectSentiment'],
+      resources: ['*'],
+    });
+    this.sentimentLambda.addToRolePolicy(allowComprehend);
+
+    // Task for the step function
+    this.detectSentiment = new LambdaInvoke(this, detectSentimentId, {
+      lambdaFunction: this.sentimentLambda,
+      resultPath: '$.sentimentResult',
+    });
   }
 }
